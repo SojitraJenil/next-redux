6506b62306d55efbf8c9547de7f0a00afae86d49
SojitraJenil <jenilsojitra4@gmail.com>
2024-08-24 12:25:06 +0530

    backend-next

--- apiClient.ts
+++ apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "/api", // Use relative URL to work with both development and production
+  baseURL: "http://localhost:3001/api",
   headers: {
     "Content-Type": "application/json",
   },
