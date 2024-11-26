4df1ea663c4267d3ffdbf859fda31ce92b7ee4f0
tejas <tejas.tech573@gmail.com>
2024-11-26 11:34:52 +0530

    login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Login from "../../components/Login/Login";
 function index() {
   return (
     <div>
       <Navbar />
       <br />
       <br />
+      <Login />
     </div>
   );
 }
